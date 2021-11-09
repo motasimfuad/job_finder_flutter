14d861d3c5ad1a55da9f934f954233ca097aae69
Motasim Fuad <motasimfuad0@gmail.com>
2021-11-09 23:23:06 +0600

    added safearea

--- lib/screens/home/widgets/home_app_bar.dart
+++ lib/screens/home/widgets/home_app_bar.dart
@@ -6,70 +6,72 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 35, //MediaQuery.of(context).padding.top,
-        left: 25,
-        right: 25,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Welcome home',
-                style: TextStyle(
-                  color: Colors.grey[600],
-                  fontSize: 18,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome home',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'motasimfuad',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 10),
+                const Text(
+                  'motasimfuad',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 30, right: 10),
-                transform: Matrix4.rotationZ(100),
-                child: Stack(
-                  children: [
-                    Icon(
-                      Icons.notifications_none_outlined,
-                      size: 30,
-                      color: Colors.grey.shade600,
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 30, right: 10),
+                  transform: Matrix4.rotationZ(100),
+                  child: Stack(
+                    children: [
+                      Icon(
+                        Icons.notifications_none_outlined,
+                        size: 30,
+                        color: Colors.grey.shade600,
                       ),
-                    )
-                  ],
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              CircleAvatar(
-                child: Image.asset('assets/images/avatar.png'),
-                backgroundColor: Colors.white,
-                radius: 25,
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 20),
+                CircleAvatar(
+                  child: Image.asset('assets/images/avatar.png'),
+                  backgroundColor: Colors.white,
+                  radius: 25,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
